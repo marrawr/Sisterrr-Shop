6341bc039b7e3c82e58e3619abf29dfbd1064af6
marrawr <mardianaomarali00@gmail.com>
2023-06-29 01:27:09 +0800

    commit

--- admin/update-image1.php
+++ admin/update-image1.php
@@ -29,7 +29,7 @@ $_SESSION['msg']="Product Image Updated Successfully !!";
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Admin| Update Product Image</title>
+	<title>Staff | Update Product Image</title>
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="css/theme.css" rel="stylesheet">
