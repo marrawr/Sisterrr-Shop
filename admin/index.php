79fd0b82f2dd6a76716328a1f142f803a77cbe92
marrawr <mardianaomarali00@gmail.com>
2023-06-29 01:21:43 +0800

    commit

--- admin/index.php
+++ admin/index.php
@@ -35,7 +35,7 @@ exit();
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Sisterrr Shop | Admin login</title>
+	<title>Sisterrr Shop | Staff login</title>
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="css/theme.css" rel="stylesheet">
